77ecbb0cf6f8dc0f495c50544f9ea16ae34d31e7
Benno Nederkorn <benno.nederkorn@web.de>
2026-01-12 16:42:55 +0100

    added a few comments

--- src/app/ros.service.ts
+++ src/app/ros.service.ts
@@ -9,19 +9,20 @@ export class RosService {
 
   constructor() {
     this.ros = new ROSLIB.Ros({
-      url: 'https://jetdaughter-1.tail640ef6.ts.net/' // Jetson's "local" Tailscale IP
+      url: 'https://jetdaughter-1.tail640ef6.ts.net/' // Jetson's funneled Tailscale URL to http://127.0.0.1:9090
+      // This was generated with: sudo tailscale funnel -bg http://127.0.0.1:9090
     });
 
     this.ros.on('connection', () => {
-      console.log('Connected to websocket server.');
+      console.log('Connected to websocket server (Jetson).');
     });
 
     this.ros.on('error', (error) => {
-      console.log('Error connecting to websocket server: ', error);
+      console.log('Error connecting to websocket server (Jetson): ', error);
     });
 
     this.ros.on('close', () => {
-      console.log('Connection to websocket server closed.');
+      console.log('Connection to websocket server (Jetson) closed.');
     });
   }
 
